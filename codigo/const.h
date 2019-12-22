6da6338f69ba3a74a297951c8a1c3a6c3d7baf7e
Oscar Nydza <oscar.nydza@protonmail.com>
2019-12-22 19:24:46 +0100

    Añadida funcion que busca hueco y aparca y desarrollo del bucle principal

--- codigo/const.h
+++ codigo/const.h
@@ -1,7 +1,10 @@
 #ifndef _CONST_H
 #define _CONST_H
 
-#define ENTRADA 0
-#define SALIDA 1
+#define S_ENTRADA 0
+#define S_SALIDA 1
+#define V_COCHE 1
+#define V_CAMION 2
+#define VACIA -1
 
 #endif
--- codigo/parking.c
+++ codigo/parking.c
@@ -4,6 +4,7 @@
 #include "const.h"
 
 unsigned int plazas, plantas, plazasLibres;
+int **parking;
 
 int main(int argc, char **argv){
     int rank,size;
@@ -21,10 +22,9 @@ int main(int argc, char **argv){
         return -1;
     }
     plazas = atoi(argv[1]);
-
+    
     plazasLibres = plazas * plantas;
 
-    int parking[plazas][plantas];
 
     for(int i = 0;i < plazas;i++)
         for(int j = 0;i < plantas;j++)
@@ -38,11 +38,58 @@ int main(int argc, char **argv){
 
     for(;;){
         MPI_Recv(&tipoV, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &estado);
-        if(estado.MPI_TAG == ENTRADA){
-            //Alguien quiere entrar            
+        //0 = entrada
+        //1 = salida
+        if(estado.MPI_TAG == S_ENTRADA){
+            //Alguien quiere entrar    
+            fuente = estado.MPI_SOURCE;
+            int tengoSitio = buscoHuecoYAparco(tipoV, fuente);
+            while(tengoSitio == -1){
+                if(tipoV == V_COCHE)  printf("ESPERA COCHE %d hasta que haya plaza.\n", fuente);
+                if(tipoV == V_CAMION) printf("ESPERA CAMION %d hasta que haya plaza.\n", fuente);
+                //mpi_recv
+                //si recibe, busco hueco otra vez
+                tengoSitio = buscoHuecoYAparco(tipoV, fuente);
+            }
+            
+        } else if(estado.MPI_TAG == 1){
+            //Sale alguien
         }
     }
 
     MPI_Finalize();
     return 0;
+}
+int buscoHuecoYAparco(int tipoV, int idV){
+    if(plazasLibres < tipoV) return -1;
+    int i, j;
+    if(tipoV == V_COCHE){
+        for(i = 0;i < plazas;i++){
+            for(j = 0;j < plantas;j++){
+                if(parking[i][j] == VACIA){
+                    parking[i][j] = idV;
+                    printf("ENTRA COCHE %d en la plaza %d de la planta %d.\n", idV, i, j);
+                    plazasLibres--;
+                    return tipoV;
+                }
+            }
+        }
+        return -1;
+    } else if(tipoV == V_CAMION){
+        for(i = 0;i < plazas;i++){
+            for(j = 0;j < plantas;j++){
+                if(parking[i][j] == VACIA && parking[i+1][j+1] == VACIA && i != plazas-1){
+                    parking[i][j] = idV;
+                    parking[i+1][j+1] = idV;
+                    printf("ENTRA CAMION %d en la plaza %d de la planta %d.\n", idV, i, j);
+                    plazasLibres--;
+                    return tipoV;
+                }
+            }
+        }
+        return -1;
+    } else {
+        printf("Error. No es un vehículo válido.\n");
+        return -1;  
+    }
 }
