c2cfa7dfe2f81b8e8b3a95ea85e0dd4bd072f95d
Oscar Nydza <oscar.nydza@protonmail.com>
2019-12-24 08:07:43 +0100

    Añadidas explicaciones

--- codigo/const.h
+++ codigo/const.h
@@ -1,15 +1,18 @@
 #ifndef _CONST_H
 #define _CONST_H
 
+// Señales
 #define S_ENTRADA 0
 #define S_HUECO 1
 #define S_SALIDA 2
 
+// Tipos de vehiculos
 #define V_COCHE 1
 #define V_CAMION 2
 
 #define VACIA -1
 
+// Nodos
 #define N_PARKING 0
 
 #endif
